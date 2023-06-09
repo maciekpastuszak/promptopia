fb61320d971b91cfbee105dacdbf5e6d90bcb324
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-09 22:57:34 +0200

    bugfix - route in jsconfig

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ export const metadata = {
     description: "Discover & Share AI Prompts"
 }
 
-const RootLayout = () => {
+const RootLayout = ({children}) => {
   return (
     <html Lang="en">
         <body>
