e0017d63ad7757586fd2aa48001b7061a5c3167e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-10 23:18:04 +0200

    fix logo with correct props

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
@@ -7,7 +9,13 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image src="/assets/images/logo.svg"/>
+        <Image 
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
       </Link>
 
     </nav>
