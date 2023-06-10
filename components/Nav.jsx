9b0c2bcfce9819fb71984b32728de8f5fd382054
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-10 23:13:34 +0200

    add imports to nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
   return (
