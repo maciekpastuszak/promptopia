b3f861051ff510d08fbb27e92c526e4988dc943c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-10 23:15:51 +0200

    add logo to nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,12 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image src="/assets/images/logo.svg"/>
+      </Link>
+
+    </nav>
   )
 }
 
