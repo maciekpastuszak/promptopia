5e0e9455c0736814ffd3dc2d06aa6c85d2463c0c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-11 12:15:38 +0200

    add useEffect for providers in Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,19 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setPrivders();
+  }, [])
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -45,7 +57,7 @@ const Nav = () => {
           </div>
         ) : (
           <>
-          
+
           </>
         )}
       </div>
