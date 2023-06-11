880ad4bd8da47087e4c060f6d783703d541d8f9e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-11 12:03:04 +0200

    mobile navigation if user logged in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -20,7 +22,19 @@ const Nav = () => {
       </Link>
 
       {/* Mobile Navigation */}
-
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <>
+          
+          </>
+        )}
+      </div>
     </nav>
   )
 }
