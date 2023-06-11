fa700468c77829cdaa7563c6748edcad01ee44e1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-11 12:08:45 +0200

    add missing props to profile img

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -34,7 +34,13 @@ const Nav = () => {
             </button>
 
             <Link href="/profile">
-              <Image src="/assets/images/profile.svg" />
+              <Image 
+              src="/assets/images/logo.svg" 
+              width={37} 
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              />
             </Link>
           </div>
         ) : (
