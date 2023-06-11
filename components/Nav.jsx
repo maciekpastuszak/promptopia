8bb3550bb6d2534c00a79bf94f7225ab5896da80
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-11 12:36:22 +0200

    add menu with my profile in Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,7 @@ const Nav = () => {
   const isUserLoggedIn = true;
 
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
     const setProviders = async () => {
@@ -83,8 +84,20 @@ const Nav = () => {
                     height={37}
                     className="rounded-full"
                     alt="profile"
-                    onClick={() => {}}
+                    onClick={() => setToggleDropdown((prev) => !prev)}
                   />
+
+                  {toggleDropdown && (
+                    <div className="dropdown">
+                      <Link
+                        href="/profile"
+                        className="dropdown_link"
+                        onClick={() => setToggleDropdown(false)}
+                      >
+                        My Profile
+                      </Link>
+                    </div>
+                  )}
             </div>
           ) : (
             <>
