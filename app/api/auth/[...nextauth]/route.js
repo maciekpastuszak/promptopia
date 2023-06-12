1c31ef018a44a2c2cb57a0bf4c0358a45a3e41dc
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-12 15:16:06 +0200

    typo

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,7 @@ const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({ session }) {
