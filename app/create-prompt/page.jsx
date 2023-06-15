c764389bb4c2834f0e2cbb1858a1b34ad3a0492e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-15 09:30:55 +0200

    add hooks to the Form component

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,12 +1,22 @@
 "use client";
 
-import Form from '@components/Form';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  const createPrompt = async (e) => {
+    
+  }
+  
   return (
     <Form 
     
