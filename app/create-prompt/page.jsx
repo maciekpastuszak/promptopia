e70bf92dbfb343de1771d8614d8504d7c6a2a99d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-15 09:38:00 +0200

    add props to the Form component

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -14,12 +14,16 @@ const CreatePrompt = () => {
   });
 
   const createPrompt = async (e) => {
-    
+
   }
   
   return (
     <Form 
-    
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
     />
   )
 }
