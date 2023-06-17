2bafa682d1231e35eb8cd4787d4cfe5346bb3223
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-17 22:12:17 +0200

    add missing prompt to prompt route

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -8,6 +8,7 @@ export const POST = async (req, res) => {
         await connectToDB();
         const newPrompt = new Prompt({
             creator:userId, 
+            prompt,
             tag
         })
 
@@ -15,6 +16,6 @@ export const POST = async (req, res) => {
 
         return new Response(JSON.stringify(newPrompt), {status: 201})
     } catch (error) {
-        
+        return new Response("Failed to create a new prompt", {status: 500})
     }
 }
