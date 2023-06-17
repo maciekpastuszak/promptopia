1527cf58eb13ec9ec234a9340e7066c5a60caf1a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-17 22:17:59 +0200

    bugfix - add missing router and session in main view

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,6 +7,9 @@ import { useState } from 'react';
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
