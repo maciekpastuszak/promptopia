72badaf10bf583eca17f65a7b8d6259fdc20d91b
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-19 22:31:37 +0200

    typo

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -40,7 +40,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p 
-        className="ffont-inter text-sm blue-gradient cursor-pointer" 
+        className="font-inter text-sm blue-gradient cursor-pointer" 
         onClick={() => handleTagClick && handleTagClick(post.tag)} >{post.tag}</p>
     </div>
   )
