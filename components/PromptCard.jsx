0a6986072405838969442b6edadb95b25b0aa94e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-19 22:17:19 +0200

    add wrapper an an image to PromptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,7 +7,19 @@ import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
-    <div>PromptCard</div>
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div>
+          <Image 
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
