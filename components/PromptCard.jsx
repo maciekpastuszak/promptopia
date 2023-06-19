54604a979a06511157133803e3e3dafaa14ad5b6
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-19 22:31:19 +0200

    add click property to the tag in PromptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -39,7 +39,9 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       </div>
 
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p className="ffont-inter text-sm blue-gradient cursor-pointer">{post.tag}</p>
+      <p 
+        className="ffont-inter text-sm blue-gradient cursor-pointer" 
+        onClick={() => handleTagClick && handleTagClick(post.tag)} >{post.tag}</p>
     </div>
   )
 }
