c1962d2bed050160f1b10bc65d8d055a7e77ff2a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-19 22:20:21 +0200

    add person who created prompt to Promptcard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,7 +9,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div>
+        <div className="flex-1 flex justify-start items-cente gap-3 cursor-pointer">
           <Image 
             src={post.creator.image}
             alt="user_image"
@@ -17,6 +17,11 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             height={40}
             className="rounded-full object-contain"
           />
+
+          <div className="flex flex-col">
+            <h3>{post.creator.username}</h3>
+            <p>{post.creator.email}</p>
+          </div>
         </div>
       </div>
     </div>
