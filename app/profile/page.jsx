c1b0a630a455d3123a31b3a5e9c1b1fe08f28045
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-20 12:30:21 +0200

    add props to Profile component

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,9 +6,22 @@ import React, { useEffect, useState } from 'react';
 import Profile from '@component/profile';
 
 const Profile = () => {
+
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = async () => {
+
+    }
+
   return (
     <Profile 
-        
+        name="My"
+        desc="Welcome to your personalized profile page"
+        data={[]}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
     />
   )
 }
