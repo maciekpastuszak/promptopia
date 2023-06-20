072aee92843a88cde722d368b81d490a1f43deee
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-20 12:36:53 +0200

    typo

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,7 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
-import Profile from '@component/profile';
+import Profile from '@components/Profile';
 
 const MyProfile = () => {
 
