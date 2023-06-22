677dffe9fdc469cc6eebb2f88a96075d496641a1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-22 13:05:04 +0200

    add welcome message and description to PromptCard

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -3,7 +3,10 @@ import PromptCard from "./PromptCard"
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className="w-full">
-      
+      <h1 className="head-text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left">{desc}</p>
     </section>
   )
 }
