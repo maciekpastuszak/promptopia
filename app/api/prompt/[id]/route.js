8af6cce3fdf2efd86dc7b5e637c40a184a18f287
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-24 23:07:06 +0200

    correct get route for a prompt

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -10,7 +10,7 @@ export const GET = async (request, { params }) => {
 
         if(!prompt) return new Response("Prompt not found", {status: 404});
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompt), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
