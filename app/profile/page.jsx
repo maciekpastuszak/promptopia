9e52dba935d6b558748c0a0c5952cbeb69e7e510
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-26 22:36:00 +0200

    bugfix

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -34,7 +34,7 @@ const MyProfile = () => {
             method: 'DELETE'
           });
 
-          const filteredPosts = myPost.filter((p) => p._id !== post._id);
+          const filteredPosts = posts.filter((p) => p._id !== post._id);
           setPosts(filteredPosts);
         } catch (error) {
           console.log(error)
