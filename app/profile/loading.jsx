d537e3a05b719f405a757f2789b5e476c1f25cd2
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-01 22:27:37 +0200

    add loading component

---
+++ app/profile/loading.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Loading = () => {
+  return (
+    <div className='w-full flex-center'>
+      <Image
+      />
+    </div>
+  );
+};
+
+export default Loading;
