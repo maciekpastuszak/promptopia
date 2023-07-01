18e9f27b328f4ca56ca10456eba058a08648abd9
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-01 22:24:10 +0200

    add user profile path

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const UserProfile = ({ params }) => {
+
+};
+
+export default UserProfile;
