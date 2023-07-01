bb2dc734e736dbb3c6377cc22611da69e4eb5eff
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-01 22:25:37 +0200

    add hooks to user profile

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -6,7 +6,25 @@ import { useSearchParams } from "next/navigation";
 import Profile from "@components/Profile";
 
 const UserProfile = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
 
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`);
+      const data = await response.json();
+
+      setUserPosts(data);
+    };
+
+    if (params?.id) fetchPosts();
+  }, [params.id]);
+
+  return (
+   <div>userProfile</div>
+  );
 };
 
 export default UserProfile;
